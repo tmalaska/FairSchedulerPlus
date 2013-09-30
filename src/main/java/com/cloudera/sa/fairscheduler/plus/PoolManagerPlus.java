3c88a25d7c0e57ef49b23279011f4b0bafc6fa8c
tmalaska <ted.malaska@cloudera.com>
2013-09-30 12:13:46 -0400

    Updated for 4.4
    added logging and fixed JT UI bug

--- src/main/java/com/cloudera/sa/fairscheduler/plus/PoolManagerPlus.java
+++ src/main/java/com/cloudera/sa/fairscheduler/plus/PoolManagerPlus.java
@@ -18,7 +18,6 @@ import org.apache.hadoop.security.UserGroupInformation;
  * pool.
  * 
  * @author ted.malaska
- *
  */
 public class PoolManagerPlus extends PoolManager {
 
@@ -28,25 +27,45 @@ public class PoolManagerPlus extends PoolManager {
 	
 	public PoolManagerPlus(FairScheduler scheduler) {
 		super(scheduler);
-		LOG.info("PoolManagerPlus 0.2 was successfully constructed");
+		LOG.info("PoolManagerPlus 0.3 was successfully constructed");
 	}
-
+	
+	public Pool getPool(String name) {
+		Pool pool = super.getPool(name);
+		LOG.debug("getPool String:" + pool.getName());
+		return pool;
+	}
+	
+	public Pool getPool(JobInProgress job) {
+		Pool pool = super.getPool(job);
+		LOG.debug("getPool Job:" + pool.getName());
+		return pool;
+	}
+	
 	public String getPoolName(JobInProgress job) {
 
 		//Get all pool names
 		Set<String> poolSet = this.getDeclaredPools();
 		String superPoolName = super.getPoolName(job);
 		
+		LOG.debug("superPoolName:" + superPoolName);
+		for (String s: poolSet) {
+			LOG.debug("-poolSet:" + s);
+		}
+		
 		String resultingPoolName = superPoolName;
 		
 		if (poolSet.contains(superPoolName) == false) {
 			//Get the user from the job config
 			String user = job.getUser();
-						
+			LOG.debug("getUser:" + superPoolName);			
+			
 			//Get the user groups from the local OS
 			try {
 				user = UserGroupInformation.createProxyUser(user,
 						UserGroupInformation.getLoginUser()).getUserName();
+			
+				LOG.debug("userName:" + user);
 				
 				if (poolSet.contains(user) == true) {
 					resultingPoolName = user;
@@ -54,22 +73,22 @@ public class PoolManagerPlus extends PoolManager {
 					String[] groups = UserGroupInformation.createProxyUser(user,
 							UserGroupInformation.getLoginUser()).getGroupNames();
 					for (String group: groups) {
+						
+						LOG.debug("group:" + group);
+						
 						if (poolSet.contains(group) == true) {
 							resultingPoolName = group;
 							break;
 						}
 					}
 				}
-				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
 		}
+		LOG.debug("return:" + resultingPoolName);
 		
 		return resultingPoolName;
-		
 	}
-
 }
